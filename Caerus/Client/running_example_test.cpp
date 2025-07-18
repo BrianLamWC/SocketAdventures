c308610b31b103ba790dc5ced2441a4cbdd93a53
BrianLamWC <brianlamwc@gmail.com>
2025-07-18 15:49:57 +0900

    test

--- Caerus/Client/running_example_test.cpp
+++ Caerus/Client/running_example_test.cpp
@@ -266,7 +266,7 @@ void senderThread(int thread_id)
         std::this_thread::yield();
     }
 
-    while (sent_count.load(std::memory_order_relaxed) < 10000)
+    while (sent_count.load(std::memory_order_relaxed) < 30000)
     {
         TxnSpec txn = generateTxn();
         int fd = my_conns[txn.hostname];
