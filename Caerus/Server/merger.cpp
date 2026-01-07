53fe12d7377bc6bd95c897af50ae1f0ee482810e
BrianLamWC <brianlamwc@gmail.com>
2026-01-07 23:39:37 +0900

    asd

--- Caerus/Server/merger.cpp
+++ Caerus/Server/merger.cpp
@@ -218,6 +218,7 @@ void Merger::insertAlgorithm()
 
             if (curr_txn == nullptr)
             {
+                curr_txn = &txn;
                 txn.setExpectedRegions(expected_regions);
                 txn.addSeenRegion(sid);
                 graph.addNode(std::make_unique<Transaction>(txn));
@@ -239,12 +240,11 @@ void Merger::insertAlgorithm()
                 auto mrw = graph.getNode(mrw_id);
 
                 if (mrw_id.empty()) { // no previous writer
-                    graph.add_MRR(data_item, txn.getID());
+                    graph.add_MRR(data_item, curr_txn->getID());
 
-                } else if (mrw and mrw->getID() != txn.getID()) { // previous writer in graph
+                } else if (mrw and mrw->getID() != curr_txn->getID()) { // previous writer in graph
                     graph.addNeighborOut(curr_txn, mrw);
-                    graph.add_MRR(data_item, txn.getID());
-
+                    graph.add_MRR(data_item, curr_txn->getID());
                 } else if (!mrw) { // previous writer not in graph
                     std::cout << "INSERT::READSET: previous writer " << mrw_id << " not in graph" << std::endl;
                     graph.removeTransaction(mrw);
